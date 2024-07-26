869cdc8e8633217ed564fb85993715d22de63d1c
Nick Rabone <n.rabone72@aol.com>
2024-07-26 19:20:11 +0100

    Seperated JS

---
+++ 04-Sidebar-Menu/script.js
@@ -0,0 +1,9 @@
+function DisplayMenu() {
+        let topNav = document.getElementById('my-topnav');
+        if (topNav.className === 'topnav') {
+            topNav.className += " responsive";
+        } else {
+            topNav.className = 'topnav'
+        }
+        
+    }
