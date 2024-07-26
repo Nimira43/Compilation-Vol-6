25cca7634423d0b76157a579721ba46ca38a464a
Nick Rabone <n.rabone72@aol.com>
2024-07-26 19:37:59 +0100

    Updated script.

--- 04-Sidebar-Menu/script.js
+++ 04-Sidebar-Menu/script.js
@@ -1,9 +1,8 @@
-function DisplayMenu() {
+const DisplayMenu = () => {
         let topNav = document.getElementById('my-topnav');
         if (topNav.className === 'topnav') {
             topNav.className += " responsive";
         } else {
             topNav.className = 'topnav'
         }
-        
     }
